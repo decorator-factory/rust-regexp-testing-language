88a358387b9b296e4ebb086c35761f9ef2a398b2
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-11-15 18:16:18 +0300

    implement matching logic for RegexpSpec

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,32 @@ enum RegexpSpec<'a> {
     },
 }
 
+use RegexpSpec::*;
+
 trait Regexp {
     fn detect(&self, test: &str) -> bool;
 
     fn find<'t>(&self, haystack: &'t str) -> Option<&'t str>;
 
     fn replace(&self, input: &str, replacer: &str) -> String;
+}
+
+impl<'a> RegexpSpec<'a> {
+    pub fn is_test_passing<R: Regexp>(&self, target: &R) -> bool {
+        match self {
+            Detects(matches) => matches.iter().all(|test| target.detect(test)),
+
+            DoesNotDetect(matches) => !matches.iter().any(|test| target.detect(test)),
+
+            InsideFinds(pairs) => pairs
+                .iter()
+                .all(|(haystack, needle)| target.find(haystack) == Some(needle)),
+
+            ReplacesWith { replacer, cases } => cases
+                .iter()
+                .all(|(input, output)| target.replace(input, replacer) == *output),
+        }
+    }
 }
 
 fn main() {
